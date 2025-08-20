d99c82ed0fb9f80f927b2e4b6e7b47e6648de62c
Sergio Morales Esquivel <161379392+smorales-gap@users.noreply.github.com>
2025-08-20 16:12:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sqlalchemy
 from decimal import Decimal
 from flask import Flask, request, jsonify
 from google.cloud import aiplatform
-from vertexai.generative_models import GenerativeModel, Tool, FunctionDeclaration, Part
+from vertexai.generative_models import GenerativeModel, Tool, FunctionDeclaration, Part, Content
 
 # --- Initialize Flask app ---
 app = Flask(__name__)
@@ -137,6 +137,7 @@ def agent():
 if __name__ == "__main__":
 
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
 
 
 
